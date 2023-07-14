020c7b890cad4f23151ab1914ac52ab8078f99a1
francesco <s304910@studenti.polito.it>
2023-07-14 20:21:07 +0200

    adding files basecall only fasta

--- sbonito/find_ref.py
+++ sbonito/find_ref.py
@@ -9,18 +9,31 @@ if __name__ == "__main__":
 
     fast5_list="./sbonito/inter_task_test_reads.txt"
 
-    specie_list=set()
+    specie_list={}
     with open(fast5_list, 'r') as f:
             for line in f:
-                specie_list.add(line.rsplit('/',2)[0])
+                specie=line.rsplit('/',3)[0]
+                if specie in specie_list:
+                    specie_list[specie].append(line)
+                else:
+                    specie_list[specie]=[line]
+
+    with open("inter_test_cleaned.txt","w") as fout:
     
-    cnt=0
-    no_fasta=0
-    for specie_dir in specie_list:
-        cnt+=1
-        if not os.path.exists(os.path.join(specie_dir,"read_references.fasta")):
-            no_fasta+=1
-            print(cnt," no fasta for: ", specie_dir.split("/"[-1]))
+        cnt=0
+        no_fasta=0
+        for specie_dir in specie_list.keys():
+            cnt+=1
+            if not os.path.exists(os.path.join(specie_dir,"read_references.fasta")):
+                no_fasta+=1
+                print(cnt," no fasta for: ", specie_dir.split("/"[-1]))
+            #else:
+            #    for read in specie_list[specie_dir]:
+            #        fout.write(read)
+            if os.path.exists(specie_dir):
+                for read in specie_list[specie_dir]:
+                    fout.write(read)
+             
     
     print(no_fasta)
     print(cnt)
